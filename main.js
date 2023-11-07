86278ebfef6a29aa5bd87ff2a07bca59a832635a
gambalongadev <gambalongadev@gmail.com>
2023-11-07 19:55:28 +0100

    adding local storage

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const tasksList = document.querySelector("#tasks-list");
 const newTaskInput = document.querySelector("#new-task-input");
 const addTaskButton = document.querySelector("#add-task-button");
 
-
 const tasks = [];
 
 const app = {
@@ -10,6 +9,20 @@ const app = {
   tasksList,
   newTaskInput,
 };
+
+window.onload = function () {
+  const savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  app.tasks = savedTasks.map((task) => {
+    return createTask(task.title, task.isCompleted);
+  });
+  app.tasks.forEach((task) => {
+    return addTaskToList(task, app.tasksList);
+  });
+};
+
+function saveTasksToLocalStorage(tasks) {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
 
 function createTask(title, isCompleted = false) {
   return {
@@ -34,7 +47,6 @@ function addTask(app) {
   app.newTaskInput.value = "";
 }
 
-
 function createTaskElement(task) {
   const taskElement = document.createElement("li");
 
@@ -47,14 +59,22 @@ function createTaskElement(task) {
     saveTasksToLocalStorage(app.tasks);
   });
 
-     const taskText = document.createElement("span");
-     taskText.textContent = task.title;
-     taskText.classList.toggle("completed", task.isCompleted);
+  const taskText = document.createElement("span");
+  taskText.textContent = task.title;
+  taskText.classList.toggle("completed", task.isCompleted);
 
   const taskDeleteButton = document.createElement("button");
   taskDeleteButton.textContent = "Eliminar";
   taskDeleteButton.className = "delete-button";
-  taskDeleteButton.addEventListener("click", () => {});
+  taskDeleteButton.addEventListener("click", () => {
+    taskElement.remove();
+
+    const taskIndex = app.tasks.indexOf(task);
+    if (taskIndex > -1) {
+      app.tasks.splice(taskIndex, 1);
+    }
+    saveTasksToLocalStorage(app.tasks);
+  });
 
   taskElement.appendChild(taskCheckbox);
   taskElement.appendChild(taskText);
@@ -65,4 +85,10 @@ function createTaskElement(task) {
 
 addTaskButton.addEventListener("click", () => {
   addTask(app);
+});
+
+newTaskInput.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    addTask(app);
+  }
 });
